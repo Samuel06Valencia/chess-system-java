26a1f080649856bfca3140ca22897f71abe39107
Samuel Silva Valencia <sv807227@gmail.com>
2023-12-21 23:22:35 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		System.out.println("Hello word!");
-		System.out.println("Seja bem-vindo");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,38 @@
+package boardgame; //Pacote correspondente a camada de tabuleiro.
+
+public class Position {
+	private int row;
+	private int column;
+	
+	
+	public Position() {
+		super();
+	}
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	//Métodos:
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
